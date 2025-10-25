eecec9650565933c79165f195531f4569eec35cb
NH5 <NH5wuzheng@outlook.com>
2025-10-25 10:49:35 +0800

    fix logs bug

--- training.py
+++ training.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # 超参数
-    batch_size = 32
+    batch_size = 16
     epochs = 30
     lr = 0.01
     use_multi_gpu = False
